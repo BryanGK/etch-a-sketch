278c712ec160a759ff47ac635678ad37c06b00ab
BryanGK <bryangordonkrauss@gmail.com>
2021-01-17 13:14:28 -0800

    Got a grid working. Next will be changing class with mouseover.

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple version of etch-a-sketch for The Odin Project.
+
+Started Jan 17, 2021.s
---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('#container');
+
+
+function makeGrid(size) {
+    for (let r = 1; r <= size; r++) {
+        const row = document.createElement('div');
+        for (let i = 1; i <= size; i++) {
+            const grid = document.createElement('div');
+            grid.style.border = "1px solid black";
+            grid.classList.add('grid');
+
+            container.appendChild(grid);
+        }
+        row.classList.add('row');
+        container.appendChild(row);
+    }
+}
+
+// function setGridSize(size) {
+//     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+// }
+
+// setGridSize(5);
+makeGrid(10);
