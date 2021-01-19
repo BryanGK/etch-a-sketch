ce06d39035ba58ff29b4165ed9f7abc361c21e6d
BryanGK <bryangordonkrauss@gmail.com>
2021-01-18 18:04:40 -0800

    Fumbling around stuck on changing the grid colors.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('#container');
 
 
+
+
 function makeGrid(size) {
     for (let r = 1; r <= size; r++) {
         const row = document.createElement('div');
@@ -16,9 +18,17 @@ function makeGrid(size) {
     }
 }
 
+
+
 // function setGridSize(size) {
 //     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 // }
 
 // setGridSize(5);
-makeGrid(10);
+makeGrid(20);
+
+const pixel = document.querySelectorAll('div');
+
+pixel.addEventListener('click', () => {
+    pixel.forEach
+});
