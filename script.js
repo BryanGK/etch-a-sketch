dc7fb78d6aa48ce30496e84e75d2fc18b9e4316b
BryanGK <bryangordonkrauss@gmail.com>
2021-01-19 13:38:38 -0800

    Added in reset button, changed from changing backround color to changing class name.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const container = document.querySelector('#container');
 
 function makeGrid(size) {
     for (let r = 1; r <= size; r++) {
-        const row = document.createElement('div');
+        const row = document.createElement('span');
         for (let i = 1; i <= size; i++) {
-            const grid = document.createElement('div');
-            grid.style.border = "1px solid black";
+            const grid = document.createElement('span');
+            // grid.style.border = "1px solid black";
             grid.classList.add('grid');
 
             container.appendChild(grid);
@@ -19,6 +19,13 @@ makeGrid(20);
 
 const pixel = document.querySelectorAll('.grid').forEach(pixel => {
     pixel.addEventListener('mouseover', () => {
-        pixel.style.background = "black";
+        pixel.classList.add('hover');
+    });
+});
+
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', () => {
+    const pixel = document.querySelectorAll('.hover').forEach(pixel => {
+        pixel.classList.remove('hover');
     });
 });
